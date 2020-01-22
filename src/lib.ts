da57b21cdcc01babbc08b4a4a51994248a94fd82
Dimitri <dimitri@mathrix.fr>
2020-01-22 14:39:23 +0100

    feat: generateKeys

--- src/lib.ts
+++ src/lib.ts
@@ -1,10 +1,14 @@
-export type Key = boolean[];
+export type Key = number;
 
-export function generateKeys(size) {
-
+/**
+ *
+ * @param length The length of the key
+ */
+export function generateKeys(length): Key[] {
+    const arraySize = Math.pow(2,length);
+    return Array.from(Array(arraySize).keys());
 }
 
 
 export function getSimulation(key: Key, message): number[] {
-
 }
